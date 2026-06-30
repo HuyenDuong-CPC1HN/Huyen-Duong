62074adbd238c769e874ce1fd0703b050acf3f15
HuyenDuong-CPC1HN <huyenduongkhohcm@gmail.com>
2026-06-30 20:01:33 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/n8n-webhook': {
-        target: 'https://n8n.cpc1hn.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/n8n-webhook/, ''),
-      },
-    },
-  },
+  base: '/Huyen-Duong/',
+  plugins: [react()],
 })
