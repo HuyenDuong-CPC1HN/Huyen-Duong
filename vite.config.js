fb477fd5e87a6d217757722bd80f4ab5a67b114d
HuyenDuong-CPC1HN <huyenduongkhohcm@gmail.com>
2026-06-30 20:35:31 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/Huyen-Duong/',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
